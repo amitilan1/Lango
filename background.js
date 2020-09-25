9ffe252c2eb3755f93ce6bb87fc245163f9bf1ce
amit.ilan <amit.ilan@mail.huji.ac.il>
2020-09-25 17:29:50 +0300

    -

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
 
 var context_id = -1;
 
+console.log("bg running");
+
 chrome.input.ime.onFocus.addListener(function (context) {
     context_id = context.contextID;
+    console.log("focus");
 });
 
 chrome.input.ime.onKeyEvent.addListener(
     function (engineID, keyData) {
+        console.log("key event");
         if (keyData.type == "keydown" && keyData.key.match(/^[a-z]$/)) {
+                console.log("small letter");
             chrome.input.ime.commitText({
                 "contextID": context_id,
                 "text": keyData.key.toUpperCase()
